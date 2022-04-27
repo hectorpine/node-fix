f2b8a9875a4804b35afe81442b41f34788921de4
hectorpine <hectorprogrammingpractice@gmail.com>
2022-04-27 16:13:09 -0500

    build settings

--- server/server.js
+++ server/server.js
@@ -346,9 +346,9 @@ app.use(
 );
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  res.sendFile(path.join(__dirname, '/client/build/index.html'))
 );
 
 app.use((err, req, res, next) => {
